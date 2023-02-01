309260b78174fca6cf2517f0e7fd93a84504ad14
Brian Nelson <brian@treegecko.com>
2023-01-31 21:04:49 -0700

    setting up services

--- src/libraries/Naiad.Libraries.System.LiteDb/Providers/LiteDbRepositoryProvider.cs
+++ src/libraries/Naiad.Libraries.System.LiteDb/Providers/LiteDbRepositoryProvider.cs
@@ -1,14 +1,23 @@
 ﻿using LiteDB;
 using Naiad.Libraries.Core.Interfaces;
+using Naiad.Libraries.System.Interfaces.System;
 
 namespace Naiad.Libraries.System.LiteDb.Providers;
 
 public class LiteDbRepositoryProvider : IRepositoryProvider
 {
     public LiteDbRepositoryProvider(
-        string filename)
+        ILiteDatabase database)
     {
+        Database = database;
+
         var mapper = BsonMapper.Global;
+
+        // Metadata
+        mapper.Entity<IDbRecord>()
+            .Id(x => x.Id);
     }
+
+    public ILiteDatabase Database { get; }
 }
 
--- src/libraries/Naiad.Libraries.System/Services/MetadataService.cs
+++ src/libraries/Naiad.Libraries.System/Services/MetadataService.cs
@@ -1,19 +1,217 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Naiad.Libraries.System.Constants.MetadataManagement;
+using Naiad.Libraries.System.Interfaces.MetadataManagement;
+using Naiad.Libraries.System.Models.MetadataManagement;
 
 namespace Naiad.Libraries.System.Services;
 
 public class MetadataService
 {
-    public MetadataService()
-    {
+    private readonly ICategorizationRepo _categorizationRepo;
+    private readonly IDataPointerRepo _dataPointerRepo;
+    private readonly IGranularityRepo _granularityRepo;
+    private readonly IMetadataPropertyRepo _metadataPropertyRepo;
+    private readonly IMetadataRepo _metadataRepo;
+    private readonly IRelationshipRepo _relationshipRepo;
+    private readonly IZoneRepo _zoneRepo;
 
+    public MetadataService(
+        ICategorizationRepo categorizationRepo,
+        IDataPointerRepo dataPointerRepo,
+        IGranularityRepo granularityRepo,
+        IMetadataPropertyRepo metadataPropertyRepo,
+        IMetadataRepo metadataRepo,
+        IRelationshipRepo relationshipRepo,
+        IZoneRepo zoneRepo)
+    {
+        _categorizationRepo = categorizationRepo;
+        _dataPointerRepo = dataPointerRepo;
+        _granularityRepo = granularityRepo;
+        _metadataPropertyRepo = metadataPropertyRepo;
+        _metadataRepo = metadataRepo;
+        _relationshipRepo = relationshipRepo;
+        _zoneRepo = zoneRepo;
     }
 
+    /* Categorization */
+    public Categorization GetCategorization(Guid id)
+    {
+        return _categorizationRepo.GetById(id);
+    }
 
+    public void Save(Categorization categorization)
+    {
+        _categorizationRepo.Save(categorization);
+    }
+
+    public IEnumerable<Categorization> GetCategorizations()
+    {
+        return _categorizationRepo.GetAll();
+    }
+
+    /* Data Pointer */
+    public DataPointer GetDataPointer(Guid id)
+    {
+        return _dataPointerRepo.GetById(id);
+    }
+
+    public void Save(DataPointer dataPointer)
+    {
+        _dataPointerRepo.Save(dataPointer);
+    }
+
+    /* Granularity */
+    public Granularity GetGranularity(Guid id)
+    {
+        return _granularityRepo.GetById(id);
+    }
+
+    public IEnumerable<Granularity> GetGranularities()
+    {
+        return _granularityRepo.GetAll();
+    }
+
+    public void Save(Granularity granularity)
+    {
+        _granularityRepo.Save(granularity);
+    }
+
+    /* Metadata Property */
+    public MetadataProperty GetMetadataProperty(Guid metadataPropertyId)
+    {
+        return _metadataPropertyRepo.GetById(metadataPropertyId);
+    }
+
+    public IEnumerable<MetadataProperty> GetMetadataProperties(Guid metadataId)
+    {
+        return _metadataPropertyRepo.Get(metadataId);
+    }
+
+    public void Save(MetadataProperty metadataProperty)
+    {
+        _metadataPropertyRepo.Save(metadataProperty);
+    }
+
+    /* Metadata */
+    public Metadata GetMetadata(Guid metadataId)
+    {
+        return _metadataRepo.GetById(metadataId);
+    }
+
+    public void Save(Metadata metadata)
+    {
+        _metadataRepo.Save(metadata);
+    }
+
+    /* Relationship */
+    public Relationship GetRelationship(Guid relationshipId)
+    {
+        return _relationshipRepo.GetById(relationshipId);
+    }
+
+    public IEnumerable<Relationship> GetChildRelationships(Guid parentId)
+    {
+        return _relationshipRepo.GetChildren(parentId);
+    }
+
+    public IEnumerable<Relationship> GetParentRelationships(Guid childId)
+    {
+        return _relationshipRepo.GetParents(childId);
+    }
+
+    public void Save(Relationship relationship)
+    {
+        _relationshipRepo.Save(relationship);
+    }
+
+    public IEnumerable<Metadata> GetChildrenMetadata(Guid parentId)
+    {
+        var output = new List<Metadata>();
+
+        var relationships = GetChildRelationships(parentId);
+
+        foreach (var relationship in relationships)
+        {
+            if (relationship.ChildType == EntityType.Metadata)
+            {
+                var metadata = GetMetadata(relationship.ChildId);
+                output.Add(metadata);
+            }
+        }
+
+        return output;
+    }
+
+    public IEnumerable<Metadata> GetParentMetadata(Guid childId)
+    {
+        var output = new List<Metadata>();
+
+        var relationships = GetParentRelationships(childId);
+
+        foreach (var relationship in relationships)
+        {
+            if (relationship.ParentType == EntityType.Metadata)
+            {
+                var metadata = GetMetadata(relationship.ParentId);
+                output.Add(metadata);
+            }
+        }
+
+        return output;
+    }
+
+    public IEnumerable<DataPointer> GetChildrenDataPointers(Guid parentId)
+    {
+        var output = new List<DataPointer>();
+
+        var relationships = GetChildRelationships(parentId);
+
+        foreach (var relationship in relationships)
+        {
+            if (relationship.ChildType == EntityType.DataPointer)
+            {
+                var dataPointer = GetDataPointer(relationship.ChildId);
+                output.Add(dataPointer);
+            }
+        }
+
+        return output;
+    }
+
+    public IEnumerable<DataPointer> GetParentDataPointers(Guid childId)
+    {
+        var output = new List<DataPointer>();
+
+        var relationships = GetParentRelationships(childId);
+
+        foreach (var relationship in relationships)
+        {
+            if (relationship.ParentType == EntityType.DataPointer)
+            {
+                var dataPointer = GetDataPointer(relationship.ParentId);
+                output.Add(dataPointer);
+            }
+        }
+
+        return output;
+    }
+
+    /* Zones */
+    public Zone GetZone(Guid zoneId)
+    {
+        return _zoneRepo.GetById(zoneId);
+    }
+
+    public IEnumerable<Zone> GetZones()
+    {
+        return _zoneRepo.GetAll();
+    }
+
+    public void Save(Zone zone)
+    {
+        _zoneRepo.Save(zone);
+    }
 
 }
 
